015d46b983e42709c9a4c4484b55a3c7eee34320
Manohar <manohar20century@gmail.com>
2023-05-15 17:22:46 +0530

    added ejs-layouts with header and footer files separately

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-// Import the required module
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
-// Set the port number
+const app = express();
 const port = 8000;
 
-// Create an Express application
-const app = express();
-
-// Serve static files from the 'assets' directory
+app.use(expressLayouts);
 app.use(express.static('./assets'));
 
-// Set the view engine to 'ejs' and specify the 'views' directory
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// Require the 'home' route file
 const router = require('./routes/home');
-
-// Mount the 'router' middleware at the root path
 app.use('/', router);
 
-// Start the server and listen on the specified port
 app.listen(port, (err) => {
     if (err) {
         console.log(`Error in Connecting to Server ${err}`);
