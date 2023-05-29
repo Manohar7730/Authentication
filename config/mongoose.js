002d8c5d13a2f7497241baeb608b71e6193b130c
Manohar <manohar20century@gmail.com>
2023-05-29 12:28:50 +0530

    installed and added dotenv , kept .env file secure in gitignore which contains database url, and added mongostore dependency to store users cookie , so that when server restarts user can still in profile page without disturbance

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Authentication_Project');
-
+mongoose.connect(process.env.MONGO_URL);
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,"Error in connecting to Database"));
-
-db.once('open',()=>{
-    console.log('Connected to database :: MongoDB');
+db.on('error', console.error.bind(console, 'Error in connecting to Database'));
+db.once('open', () => {
+  console.log('Connected to database :: MongoDB');
 });
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const expressLayouts = require('express-ejs-layouts');
+const env = require('dotenv').config();
 const db = require('./config/mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const session = require('express-session');
 
 const app = express();
 const port = 8000;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,25 +24,31 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(session({
-    name : 'Cookie',
-    secret : 'something',
-    saveUninitialized : false,
-    resave : false,
-    maxAge : 1000*60*10
-}))
+// Session Configuration
+app.use(
+  session({
+    name: 'Cookie',
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    maxAge: 1000 * 60 * 10,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL,autoRemove:'disabled'},function(err){
+        console.log(err || 'connect-mongodb setup ok')
+    }),
+  })
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(passport.setAuthenticatedUser)
+app.use(passport.setAuthenticatedUser);
 
 const router = require('./routes/home');
 app.use('/', router);
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(`Error in Connecting to Server ${err}`);
-    }
-    console.log(`Server is up and running on port: ${port}`);
+  if (err) {
+    console.log(`Error in Connecting to Server ${err}`);
+  }
+  console.log(`Server is up and running on port: ${port}`);
 });
