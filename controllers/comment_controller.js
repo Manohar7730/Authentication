f1de7a8b0b31169b626197612a0bb03fecc0a8cd
Manohar <manohar20century@gmail.com>
2023-06-04 10:54:53 +0530

    added router,controller,ejs for deleting comment and added checks for deleting comment who posted the comment and owner of post, and also cheared bugs in header section

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,21 +1,44 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-module.exports.create = async (req,res)=>{
-    try{
+module.exports.create = async (req, res) => {
+    try {
         const post = await Post.findById(req.body.post);
-        if(post){
+        if (post) {
             const comment = await Comment.create({
-                content : req.body.content,
-                post : req.body.post,
-                user : req.user._id
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
             });
             post.comments.push(comment);
             await post.save();
             return res.redirect('back');
         }
-    }catch(err){
-        console.log('Error in creating a comment',err)
+    } catch (err) {
+        console.log('Error in creating a comment', err)
+        return res.redirect('back');
+    }
+}
+
+module.exports.destroy = async (req, res) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return res.status(400).send('Comment not found');
+        }
+        const post = await Post.findById(req.params.postId);
+        if (!post) {
+            return res.status(400).send('Post not found');
+        }
+        if (comment.user == req.user.id || post.user == req.user.id) {
+            let postId = comment.post;
+            await comment.deleteOne();
+            const post = await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } })
+            return res.redirect('back');
+        }
+        return res.redirect('back');
+    } catch (err) {
+        console.log('Error in deleting a comment', err)
         return res.redirect('back');
     }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -7,6 +7,7 @@ const commentController = require('../controllers/comment_controller');
 const passport = require('passport');
 
 router.post('/create',passport.checkAuthentication,commentController.create);
+router.get('/delete/:postId/:commentId',passport.checkAuthentication,commentController.destroy);
 
 // Export the router to be used in other parts of the application
 module.exports = router;
