cb3390e88a9885417e5923a93493683b5d75c146
Manohar <manohar20century@gmail.com>
2023-06-04 11:33:35 +0530

    made all the users under friends with names

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = async (req, res) => {
     try{
@@ -10,9 +11,11 @@ module.exports.home = async (req, res) => {
                 path: 'user',
             },
         })
+        const user = await User.find({});
         return res.render('home', {
             title: 'Home Page',
-            posts : posts
+            posts : posts,
+            all_users : user
         });
     }catch(err){
         console.log(err,'Error in creating post');
