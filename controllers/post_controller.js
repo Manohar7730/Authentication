1481f259fe49e9bdf8b4e5f02f07c6b3c7700f6e
Manohar <manohar20century@gmail.com>
2023-06-05 12:53:53 +0530

    added flash messages for post creation and delete

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -7,9 +7,10 @@ module.exports.create  = async(req,res)=>{
             content:req.body.content,
             user : req.user._id
         })
+        req.flash('success','Post published!')
         return res.redirect('back');
     }catch(err){
-        console.log(err,'Error in creating post');
+        req.flash('error',err)
         return res.redirect('back');
     }
 }
@@ -20,12 +21,14 @@ module.exports.destroy = async(req,res)=>{
         if(post.user == req.user.id){
             post.deleteOne();
             await Comment.deleteMany({post : req.params.id});
+            req.flash('success','Posts and associated comments deleted')
             return res.redirect('back');
         }else{
+            req.flash('error','You cannot remove this post');
             return res.redirect('back');
         }
     }catch(err){
-        console.log(err,'Error in removing post');
+        req.flash('error',err)
         return res.redirect('back');
     }
 }
