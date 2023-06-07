83b5bc6a3248c6339e099663c4f2b15338d774e7
Manohar <manohar20century@gmail.com>
2023-06-07 20:29:24 +0530

    by using PostComments(postId) in home_posts file , we created a class and prevented default behaviour of add comment button and also added an id for createcomment form

--- assets/js/post_comments.js
+++ assets/js/post_comments.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-    console.log('hello');
-})
+class PostComments{
+        constructor(postId){
+            this.postId = postId;
+            this.postContainer = $(`#post-${postId}`)
+            this.newCommentForm = $(`#post-${postId}-comments-form`);
+
+            this.createComment(postId);
+        }
+        createComment(postId){
+            let self = this;
+            this.newCommentForm.submit(function (e) {
+                e.preventDefault();
+                // Handle the form submission here
+            });
+        }
+    }
