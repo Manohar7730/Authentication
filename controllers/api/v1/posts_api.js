d1559e4129163ff7111d323d3c6941d1d99bdb3d
Manohar <manohar20century@gmail.com>
2023-06-13 21:47:14 +0530

    added destroy function in api v1 posts

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
 module.exports.index = async (req,res)=>{
 
@@ -17,4 +18,20 @@ module.exports.index = async (req,res)=>{
         message : "all posts",
         posts : posts
     })
+}
+
+module.exports.destroy = async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+            await post.deleteOne();
+            await Comment.deleteMany({post : req.params.id});
+
+            return res.status(200).json({
+                message : 'post and associated comments deleted'
+            })
+        
+    }catch(err){
+        req.flash('error',err)
+        return res.status(500).json({message:'Error in deleting'})
+    }
 }
