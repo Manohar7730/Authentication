34e0cf5f4ea1e53b242cac00a074bdd818e443c4
Manohar <manohar20century@gmail.com>
2023-06-13 19:22:37 +0530

    retrieving all posts in json at api-version is working good

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,20 @@
-module.exports.index = (req,res)=>{
+const Post = require('../../../models/post');
+
+module.exports.index = async (req,res)=>{
+
+    const posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            options: { sort: {createdAt: -1}},
+            populate: {
+                path: 'user',
+            },
+        })
+
     return res.json({
         message : "all posts",
-        posts : []
+        posts : posts
     })
 }
