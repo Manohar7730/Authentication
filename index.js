335b1e346c1d6c0068ff4bb12792d84a664174b5
Manohar <manohar20century@gmail.com>
2023-08-20 13:58:39 +0530

    fixed code working of minified js code

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const env = require("./config/environment");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const expressLayouts = require("express-ejs-layouts");
-
 const morgan = require("morgan");
-const db = require("./config/mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const passport = require("passport");
+const flash = require("connect-flash");
+
+const db = require("./config/mongoose");
+const env = require("./config/environment");
 const passportLocal = require("./config/passport-local-strategy");
 const passportJWT = require("./config/passport-jwt-strategy");
 const passportGoogle = require("./config/passport-google-strategy");
-const flash = require("connect-flash");
 const customMiddleware = require("./config/middleware");
 
 const app = express();
 require("./config/view-helpers")(app);
 const port = 8000;
 console.log(env.name);
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(expressLayouts);
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.static(env.asset_path));
-
 app.use(morgan(env.morgan.mode, env.morgan.options));
 
 app.set("layout extractStyles", true);
@@ -57,17 +56,19 @@ app.use(
   })
 );
 
+// Passport Setup
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(passport.setAuthenticatedUser);
+
 app.use(flash());
 app.use(customMiddleware.setFlash);
 
+// Routes
 const router = require("./routes/home");
-const environment = require("./config/environment");
 app.use("/", router);
 
+// Start Server
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in Connecting to Server ${err}`);
