e874882b629cdfa6c854c0c843ea32323a60cdb3
Manohar <manohar20century@gmail.com>
2023-09-01 10:39:41 +0530

    Fixed bugs and errors for comment user name

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,26 +1,28 @@
-const passport = require('passport');
-const JWtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require("passport");
+const JWtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 let opts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: "authentication"
-}
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "authentication",
+};
 
-passport.use(new JWtStrategy(opts, async (jwtPayload, done) => {
+passport.use(
+  new JWtStrategy(opts, async (jwtPayload, done) => {
     try {
-        const user = await User.findById(jwtPayload._id);
-        if (user) {
-            return done(null, user)
-        } else {
-            return done(null, false)
-        }
+      const user = await User.findById(jwtPayload._id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
     } catch (err) {
-        console.log('Error in finding the user');
-        return;
+      console.log("Error in finding the user");
+      return;
     }
-}))
+  })
+);
 
 module.exports = passport;
--- index.js
+++ index.js
@@ -1,64 +1,69 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const expressLayouts = require('express-ejs-layouts');
-const env = require('dotenv').config();
-const db = require('./config/mongoose');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const passportJWT = require('./config/passport-jwt-strategy');
-const passportGoogle = require('./config/passport-google-strategy');
-const flash = require('connect-flash');
-const customMiddleware = require('./config/middleware');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const db = require("./config/mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const passportJWT = require("./config/passport-jwt-strategy");
+const passportGoogle = require("./config/passport-google-strategy");
+const flash = require("connect-flash");
+const customMiddleware = require("./config/middleware");
 
 const app = express();
 const port = 8000;
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(expressLayouts);
-app.use('/uploads',express.static(__dirname + '/uploads'))
-app.use(express.static('./assets'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(express.static("./assets"));
 
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 // Session Configuration
 app.use(
   session({
-    name: 'Cookie',
-    secret: 'something',
+    name: "Cookie",
+    secret: "authentication",
     saveUninitialized: false,
     resave: false,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URL,
-      mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-      autoRemove: 'interval',
-      autoRemoveInterval: 10
-    }, function(err){
-      console.log(err || 'connect-mongodb setup ok');
-    }),
-    cookie:{
+    store: MongoStore.create(
+      {
+        mongoUrl: process.env.MONGO_URL,
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        autoRemove: "interval",
+        autoRemoveInterval: 10,
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+    cookie: {
       maxAge: 1000 * 60 * 10,
-    }
+    },
   })
 );
 
+// Passport Setup
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(passport.setAuthenticatedUser);
+
 app.use(flash());
 app.use(customMiddleware.setFlash);
 
-const router = require('./routes/home');
-app.use('/', router);
+// Routes
+const router = require("./routes/home");
+app.use("/", router);
 
+// Start Server
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in Connecting to Server ${err}`);
