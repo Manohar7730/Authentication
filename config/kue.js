5f6aa254420e64a10d9e8e4f0dbf850561b23e0a
Manohar <manohar20century@gmail.com>
2023-09-08 20:02:02 +0530

    fixed redis

--- config/kue.js
+++ config/kue.js
@@ -1,5 +1,20 @@
 const kue = require("kue");
+const redis = require("redis");
 
-const queue = kue.createQueue();
+// Create a Redis client using the provided Render Redis instance details
+const client = redis.createClient({
+  host: "red-cjtiqsdhtt0c73e4unbg", // Replace with your actual Redis hostname
+  port: 6379, // Replace with your actual Redis port
+  password: "1234", // Replace with your actual Redis password
+});
+
+// Create a Kue queue and configure it to use the Redis client
+const queue = kue.createQueue({
+  redis: {
+    createClientFactory: function () {
+      return client;
+    },
+  },
+});
 
 module.exports = queue;
