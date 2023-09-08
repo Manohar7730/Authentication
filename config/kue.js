4a350642ffe161dfb1d7d8743cf5646570a76969
Manohar <manohar20century@gmail.com>
2023-09-08 19:39:28 +0530

    fixed minor bugs

--- config/kue.js
+++ config/kue.js
@@ -1,11 +1,5 @@
 const kue = require("kue");
 
-const queue = kue.createQueue({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  auth: {
-    password: process.env.REDIS_PASSWORD, // Use the environment variable
-  },
-});
+const queue = kue.createQueue();
 
 module.exports = queue;
