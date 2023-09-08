5153e0d5715e566597fb02b03b35386a9fdecea5
Manohar <manohar20century@gmail.com>
2023-09-08 18:23:52 +0530

    set redis password in kue

--- config/kue.js
+++ config/kue.js
@@ -3,6 +3,9 @@ const kue = require("kue");
 const queue = kue.createQueue({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+  auth: {
+    password: process.env.REDIS_PASSWORD, // Use the environment variable
+  },
 });
 
 module.exports = queue;
