81be74d4cdaf907eda630f358b77203da2497709
Manohar <manohar20century@gmail.com>
2023-09-08 19:25:21 +0530

    added redis-host,password,port on env

--- config/kue.js
+++ config/kue.js
@@ -8,4 +8,8 @@ const queue = kue.createQueue({
   },
 });
 
+console.log("REDIS_HOST:", process.env.REDIS_HOST);
+console.log("REDIS_PORT:", process.env.REDIS_PORT);
+console.log("REDIS_PASSWORD:", process.env.REDIS_PASSWORD);
+
 module.exports = queue;
